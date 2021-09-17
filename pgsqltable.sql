69d9daafec151400fc6789fdf09b619c9942e5c0
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2021-09-17 18:51:51 -0400

    Create pgsqltable.sql

---
+++ pgsqltable.sql
@@ -0,0 +1,24 @@
+CREATE TABLE iotjetsonjson 
+(
+	uuid VARCHAR(255), 
+	camera VARCHAR(255),
+	ipaddress VARCHAR(255),  
+	networktime VARCHAR(255), 
+        top1pct VARCHAR(255), 
+	top1 VARCHAR(255), 
+	cputemp VARCHAR(255), 
+	gputemp VARCHAR(255),
+        gputempf VARCHAR(255),
+	cputempf VARCHAR(255), 
+	runtime VARCHAR(255),
+	host VARCHAR(255),
+	filename VARCHAR(255),  
+	host_name VARCHAR(255), 
+        macaddress VARCHAR(255), 
+	te VARCHAR(255), 
+	systemtime VARCHAR(255),
+	cpu VARCHAR(255),
+        diskusage VARCHAR(255),
+	memory VARCHAR(255), 
+	imageinput VARCHAR(255)
+);
